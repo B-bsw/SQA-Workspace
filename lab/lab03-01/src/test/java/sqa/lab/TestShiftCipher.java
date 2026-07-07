daa020de75396446467598db2fa4294c15f180b3
BB <sorawitthassadorn15@gmail.com>
2026-07-08 00:28:59 +0700

    feat: add testing

---
+++ lab/lab03-01/src/test/java/sqa/lab/TestShiftCipher.java
@@ -0,0 +1,29 @@
+package sqa.lab;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestShiftCipher {
+    private final ShiftCipher shiftCipher = new ShiftCipher();
+
+    @Test // TC01
+    void testCase01() {
+        assertEquals("ZVMADHYL", shiftCipher.shift("SOFTWARE", 3));
+    }
+
+    @Test // TC02
+    void testCase02() {
+        assertEquals("zvmadhyl", shiftCipher.shift("software", 3));
+    }
+
+    @Test // TC03
+    void testCase03() {
+        assertEquals("", shiftCipher.shift("ABCD", 3));
+    }
+
+    @Test // TC04
+    void testCase04() {
+        assertEquals("", shiftCipher.shift("DHCP", 3));
+    }
+}
