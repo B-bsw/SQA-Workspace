238d1c48ba6887067b5134e85dc75a1eaefa01c0
BB <sorawitthassadorn15@gmail.com>
2026-07-14 14:14:47 +0700

    feat: add Lab04-01

--- LabDemo/Lab4_BVT/junit6-labdemo/src/test/java/com/example/junit6_labdemo/TestMultiplication.java
+++ LabDemo/Lab4_BVT/junit6-labdemo/src/test/java/com/example/junit6_labdemo/TestMultiplication.java
@@ -2,27 +2,28 @@ package com.example.junit6_labdemo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestMultiplication {
 
-	    //TC01: number1 = 2, number2 = 5, expected result = 10
-		@Test
-		void testmultiply() {
-			Calculator calculator = new Calculator();
-			int result = calculator.multiply(2, 5);
-				
-			assertEquals(10, result);
-		}
-			
-			
-		//TC02: number = 5, expected result = 25
-		@Test
-		void testsquare() {
-			Calculator calculator = new Calculator();
-			int result = calculator.square(5);
-				
-			assertEquals(25, result);
-		}
+    //TC01: number1 = 2, number2 = 5, expected result = 10
+    @Test
+    @DisplayName("TC01: number1 = 2, number2 = 5, expected result = 10")
+    void testmultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 5);
 
+        assertEquals(10, result);
+    }
+
+    //TC02: number = 5, expected result = 25
+    @Test
+    @DisplayName("TC02: number = 5, expected result = 25")
+    void testsquare() {
+        Calculator calculator = new Calculator();
+        int result = calculator.square(5);
+
+        assertEquals(25, result);
+    }
 }
