769c292b081ea9e808caa03bb61287c98a056c00
BB <sorawitthassadorn15@gmail.com>
2026-07-19 20:35:13 +0700

    add testing of lab04

---
+++ lab/lab04/src/test/java/TestHealthIndexScore.java
@@ -0,0 +1,37 @@
+import com.health.fitness.HealthIndexScore;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@TestMethodOrder(OrderAnnotation.class)
+public class TestHealthIndexScore {
+
+    // Normal Boundary
+    @Order(1)
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/NormalBoundary.csv")
+    @DisplayName("Normal Boundary Value Testing")
+    void testNormalBoundaryValue(String tc, int vo2Max, int rhr, int hrr) {
+        System.out.println(
+            tc + " " + new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
+        );
+    } // mvn test -Dtest=TestHealthIndexScore#testNormalBoundaryValue
+
+    // Robustness
+    @Order(2)
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Robustness.csv")
+    @DisplayName("Robustness Testing")
+    void testRobustness(String tc, int vo2Max, int rhr, int hrr) {
+        System.out.println(
+            tc + " " + new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
+        );
+    }
+    // mvn test -Dtest=TestHealthIndexScore#testRobustness
+}
