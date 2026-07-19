604e34e924cafd260bda04de756a1e999381179f
BB <sorawitthassadorn15@gmail.com>
2026-07-19 23:30:13 +0700

    Lab4: improved Testing code

--- lab/lab04/src/test/java/TestHealthIndexScore.java
+++ lab/lab04/src/test/java/TestHealthIndexScore.java
@@ -1,3 +1,6 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.health.fitness.HealthIndexScore;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.DisplayName;
@@ -17,21 +20,60 @@ public class TestHealthIndexScore {
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/NormalBoundary.csv")
     @DisplayName("Normal Boundary Value Testing")
-    void testNormalBoundaryValue(String tc, int vo2Max, int rhr, int hrr) {
-        System.out.println(
-            tc + " " + new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
-        );
+    void testNormalBoundaryValue(
+        String tc,
+        int vo2Max,
+        int rhr,
+        int hrr,
+        String expectedResult
+    ) {
+        testingHealthIndexScore(tc, vo2Max, rhr, hrr, expectedResult);
     } // mvn test -Dtest=TestHealthIndexScore#testNormalBoundaryValue
 
     // Robustness
     @Order(2)
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/Robustness.csv")
+    @CsvFileSource(
+        files = "src/test/resources/Robustness.csv",
+        numLinesToSkip = 1
+    )
     @DisplayName("Robustness Testing")
-    void testRobustness(String tc, int vo2Max, int rhr, int hrr) {
-        System.out.println(
-            tc + " " + new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
-        );
+    void testRobustness(
+        String tc,
+        int vo2Max,
+        int rhr,
+        int hrr,
+        String expectedResult
+    ) {
+        testingHealthIndexScore(tc, vo2Max, rhr, hrr, expectedResult);
+    } // mvn test -Dtest=TestHealthIndexScore#testRobustness
+
+    private void testingHealthIndexScore(
+        String tc,
+        int vo2Max,
+        int rhr,
+        int hrr,
+        String expectedResult
+    ) {
+        if (expectedResult.equals("Invalid")) {
+            assertThrows(IllegalArgumentException.class, () ->
+                new HealthIndexScore(vo2Max, rhr, hrr)
+            );
+
+            System.out.println(tc + " " + expectedResult);
+        } else {
+            assertEquals(
+                expectedResult,
+                new HealthIndexScore(vo2Max, rhr, hrr)
+                    .getFitnessLevel()
+                    .toString()
+            );
+
+            System.out.println(
+                tc +
+                    " " +
+                    new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
+            );
+        }
     }
-    // mvn test -Dtest=TestHealthIndexScore#testRobustness
 }
