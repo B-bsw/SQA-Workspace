184bc5c8e2f42c749b4371b00f4a09cc484ff251
BB <sorawitthassadorn15@gmail.com>
2026-07-20 12:44:16 +0700

    lab04: refactor robustness

--- lab/lab04/src/test/java/TestRobustness.java
+++ lab/lab04/src/test/java/TestRobustness.java
@@ -1,43 +1,41 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.health.fitness.HealthIndexScore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class TestRobustness {
+
     @ParameterizedTest
-    @CsvFileSource(
-            files = "src/test/resources/Robustness.csv",
-            numLinesToSkip = 1
-    )
+    @CsvFileSource(files = "src/test/resources/Robustness.csv")
     @DisplayName("Robustness Testing")
     void testRobustness(
-            String tc,
-            int vo2Max,
-            int rhr,
-            int hrr,
-            String expectedResult
+        String tc,
+        int vo2Max,
+        int rhr,
+        int hrr,
+        String expectedResult
     ) {
         if (expectedResult.equals("Invalid")) {
             assertThrows(IllegalArgumentException.class, () ->
-                    new HealthIndexScore(vo2Max, rhr, hrr)
+                new HealthIndexScore(vo2Max, rhr, hrr)
             );
 
             System.out.println(tc + " " + expectedResult);
         } else {
             assertEquals(
-                    expectedResult,
-                    new HealthIndexScore(vo2Max, rhr, hrr)
-                            .getFitnessLevel()
-                            .toString()
+                expectedResult,
+                new HealthIndexScore(vo2Max, rhr, hrr)
+                    .getFitnessLevel()
+                    .toString()
             );
 
             System.out.println(
-                    tc +
-                            " " +
-                            new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
+                tc +
+                    " " +
+                    new HealthIndexScore(vo2Max, rhr, hrr).getFitnessLevel()
             );
         }
     }
